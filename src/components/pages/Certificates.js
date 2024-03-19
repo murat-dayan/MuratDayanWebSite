e2d55964b944af5fcbe4093638ef433cb87f1fbf
murat-dayan <muratdayan359@gmail.com>
2024-03-19 05:34:17 +0300

    assets changed

--- src/components/pages/Certificates.js
+++ src/components/pages/Certificates.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import '../../App.css'
-import ImageCard from '../ImageCard'
-import Footer from '../Footer'
+import React from "react";
+import "../../App.css";
+import ImageCard from "../ImageCard";
+import Footer from "../Footer";
 
-
-export default function Certificates(){
-    return (
-        <>
-        <ImageCard image="images/jetpackcomposeudemysertifikasi.jpg" text="JEtoaacasd" />
-        </>
-    )
+export default function Certificates() {
+  return (
+    <>
+      <ImageCard
+        image="images/jetpackcomposeudemysertifikasi.jpg"
+        text="certificate"
+      />
+    </>
+  );
 }
