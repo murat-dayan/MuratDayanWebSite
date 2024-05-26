212cf827308e989d0800b78f697b1af601c74796
muratdayan <muratdayan359@gmail.com>
2024-05-26 23:07:07 +0300

    certificates page designed

--- src/components/pages/Certificates.js
+++ src/components/pages/Certificates.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "../../App.css";
 import ImageCard from "../ImageCard";
-import Footer from "../Footer";
+import "./Certificates.css";
 
 export default function Certificates() {
   return (
-    <>
-      <ImageCard
-        image="images/jetpackcomposeudemysertifikasi.jpg"
-        text="certificate"
-      />
-      <ImageCard image="images/fluttercert.png" text="certificate" />
-    </>
+    <div className="certificates-container">
+      <div className="image-row">
+        <ImageCard
+          image="images/jetpackcomposeudemysertifikasi.jpg"
+          text="certificate"
+        />
+        <ImageCard image="images/fluttercert.png" text="certificate" />
+      </div>
+      <div className="image-row">
+        <ImageCard image="images/android.png" text="certificate" />
+        <ImageCard image="images/image.png" text="certificate" />
+      </div>
+    </div>
   );
 }
