158abb3aec23d3f7bf9f4346728ac3a58f25367e
muratdayan <muratdayan359@gmail.com>
2024-11-20 22:15:28 +0300

    changed cv url

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './HeroSection.css';
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
-
   const handleDownloadCV = () => {
     // CV dosyanızın URL'ini buraya ekleyin
-    const cvUrl = 'https://drive.google.com/file/d/16waiJRlj125c1j4IkeNR9uEgjqo0Fmb2/view?usp=sharing';
+    const cvUrl =
+      "https://drive.google.com/file/d/1IoPcL_juKCjY20uikl6d7ibK0NcAJzoi/view?usp=sharing";
     // Yeni sekmede açılacak bir a etiketi oluşturun
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = cvUrl;
     // Dosya adını ayarlayın
-    link.download = 'muratdayancv.pdf';
+    link.download = "muratdayancv.pdf";
     // Yeni sekmede açılması için target özelliğini belirtin
-    link.target = '_blank';
+    link.target = "_blank";
     // A etiketini tıklayınca indirme işlemi başlatılır
     link.click();
   };
-  
 
   return (
-    <div className='hero-container'>
+    <div className="hero-container">
       {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
       <h1>WELCOME</h1>
       <p>I'm an android developer </p>
-      <div className='hero-btns'>
+      <div className="hero-btns">
         <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
           onClick={handleDownloadCV}
         >
           GET MY CV
